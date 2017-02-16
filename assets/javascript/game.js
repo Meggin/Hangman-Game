46adadf8ca1226a74c1969a9b77a83f1b07845c0
mkearney@google.com <mkearney@google.com>
2017-02-15 21:32:56 -0800

    progressing, slower than i'd like, but progressing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,15 @@ var StarWarsWords = {
 	word2: ["L", "E", "I", "A"]
 };
 
+var wins = 0;
+
 var wordArray = [StarWarsWords.word1, StarWarsWords.word2];
 
-//Randomly selects word from StarWarsWords object.
 var word = wordArray[Math.floor(Math.random()*wordArray.length)];
 console.log(word);
 
 //Creates a placeholder for word.
-function createWordPlaceholder(word) {
+function createWordPlaceholder(word) {	
 	var wordPlaceholder = [];
 	for (i = 0; i < word.length; i++) {
 		wordPlaceholder.push("_");
@@ -40,9 +41,37 @@ function checkLetterInWord(userInput, word, placeholder) {
 }
 */
 
+function trackLetterGuesses(guess) {
+	console.log('event.key is ' + key);
+	var lettersGuessed = [];
+	lettersGuessed = lettersGuessed.push(key);
+	console.log(lettersGuessed);
+	lettersGuessed = lettersGuessed.join(", ");
+	document.getElementById('letters-guessed').innerHTML = lettersGuessed;
+}
+
+function trackWins(wins) {
+	wins++;
+	console.log("Value of wins: " + wins);
+	document.getElementById('win-count').innerHTML = wins;
+}
+
+function restartGame() {
+	guessesLeft = 13;
+	document.getElementById('guess-count').innerHTML = guessesLeft;
+	createWordPlaceholder(word, wordArray);
+}
+
+var correctGuessCount = 0;
+var lettersGuessed = "";
+var guessesLeft = 13;
 //Function fires when letter selected.
 document.onkeyup = function(event) {
 	console.log('This is the key entered', event.key);
+	var guess = event.key;
+	guessesLeft--;
+	document.getElementById('guess-count').innerHTML = guessesLeft;
+	console.log('Guesses left' + guessesLeft);
 	var userInput = String.fromCharCode(event.keyCode).toUpperCase();
 	console.log(userInput + " should match the key entered");
 	//checkLetterInWord(userInput, word, placeholder);
@@ -50,6 +79,14 @@ document.onkeyup = function(event) {
 	  //Check to see if letter exists in word.
 	  if (userInput == word[i]) {
 	  	console.log(userInput + " is in word at " + i);
+	  	correctGuessCount++;
+	  	console.log(correctGuessCount);
+	  	console.log("Word length = " + word.length);
+	  	if (correctGuessCount == word.length) {
+	  		console.log("Woot");
+	  		trackWins(wins);
+	  		restartGame();
+	  	}
 	  	placeholderArray[i] = userInput;
 	  	console.log(userInput);
 	  	placeholder = placeholderArray.join(" ");
@@ -57,7 +94,5 @@ document.onkeyup = function(event) {
 	  }
 	}
 };
-
-//
 
 
