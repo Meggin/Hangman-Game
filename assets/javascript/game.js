6dca1516eb9d72fa77140d48ed71d4ef36c65e2b
mkearney@google.com <mkearney@google.com>
2017-02-16 15:02:09 -0800

    Slightly cleaner javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,17 @@
+// VARIABLES
+// =================================================================================
+var wins = 0;
+var placeholderArray = [];
+var prevPlaceholderArray = [];
+var wordPlaceholder = [];
+var lettersGuessed = [];
+var word = [];
+var wordPlaceholderString = "";
+var userInput = "";
+var correctGuessCount = 0;
+var guessesLeft = 13;
+
+// Object of Star Wars Hangman words.
 var StarWarsWords = {
 	word1: ["H", "A", "N", "S", "O", "L", "O"],
 	word2: ["D", "A", "R", "T", "H", "V", "A", "D", "E", "R"],
@@ -6,91 +20,119 @@ var StarWarsWords = {
 	word5: ["Y", "O", "D", "A"]
 };
 
-var wins = 0;
-
-var placeholderArray = [];
-
-var prevPlaceholderArray = [];
-
-var wordPlaceholder = [];
-
-var lettersGuessed = [];
-
-var word = [];
-
-var wordPlaceholderString = "";
-
+// Array of Star Wars Hangman words created from object.
 var wordArray = [StarWarsWords.word1, StarWarsWords.word2, StarWarsWords.word3, StarWarsWords.word4, StarWarsWords.word5];
 
-userInput = "";
+// Initialize game on window load.
+createWord(wordArray);
 
+// MAIN PROCESS
+// =================================================================================
+document.onkeyup = function(event) {
+	console.log('This is the key entered', event.key);
+	var keyPress;
+
+	if (typeof event != 'undefined') {
+		keyPress = event.keyCode;
+
+		// Convert user input key to upper case string.
+		userInput = String.fromCharCode(keyPress).toUpperCase();
+		console.log(userInput + " should match the key entered");
+
+		// Track user guesses over time.
+		trackLetterGuesses(userInput);
+
+		// Build hangman word based on new user input.
+		buildWord(userInput);
+	}
+
+	// No idea what this does, but seems to be needed.
+	else if (e) {
+		keyPress = e.which;
+	}
+	return false;
+};
+
+// FUNCTIONS
+// =================================================================================
+
+//Create array from randomly selected Star Wars word Array.
 function createWord(wordArray) {
 	word = wordArray[Math.floor(Math.random()*wordArray.length)];
 	console.log(word);
+
+	//Create placeholder for word in UI.
 	createWordPlaceholder(word);
 	return word;
 };
 
-createWord(wordArray);
-
-//Creates a placeholder for word.
+//Create placeholder for word in UI.
 function createWordPlaceholder(word) {	
 	var wordPlaceholder = [];
+
+	// Fill array with underscores.
 	for (i = 0; i < word.length; i++) {
 		wordPlaceholder.push("_");
 	}
+
+	// Convert word placeholder array to string for displaying in UI.
 	wordPlaceholderString = wordPlaceholder.join(" ");
+
+	// Display word placeholder in UI.
 	document.getElementById('word-placeholder').textContent = wordPlaceholderString;
 	return wordPlaceholder;
 };
 
+// Keep track of user guesses.
 function trackLetterGuesses(userInput) {
-	guessesLeft--;
-	if (guessesLeft == 0) {
-		correctGuessCount = 0;
-		createWord(wordArray);
-		userInput = "";
-		prevPlaceholderArray = [];
-		restartGame();
-	}
-	console.log('Guess is ' + userInput);
+
+	/* 
+	 * Check if letter already guessed.
+	 * Don't track letters more than once.
+	 */
 	for (i = 0; i < lettersGuessed.length; i++) {
 		if (userInput == lettersGuessed[i]) {
 			return;
 		}
 	}
+
+	// Push letter guessed.
 	lettersGuessed.push(userInput);
 	console.log("LettersGuessed array item: " + lettersGuessed[0]);
+	
+	// Convert letters guessed array to string for displaying in UI.
 	var lettersGuessedString = lettersGuessed.join(", ");
 	document.getElementById('letters-guessed').innerHTML = lettersGuessedString;
+
+	// Each guess reduces number of guesses left. 
+	guessesLeft--;
+
+	// Display guesses left in UI.
 	document.getElementById('guess-count').innerHTML = guessesLeft;
 	console.log('Guesses left' + guessesLeft);
+
+
+	// Game restarts when no more guesses left.
+	if (guessesLeft == 0) {
+		restartGame();
+	}
+
 	return lettersGuessedString;
 };
 
-function trackWins(wins) {
-	wins++;
-	console.log("Value of wins: " + wins);
-	return wins;
-};
-
-function restartGame(wordPlaceholder) {
-	//createWord(wordArray);
-	placeholderArray = [];
-	guessesLeft = 13;
-	correctGuessCount = 0;
-	document.getElementById('guess-count').innerHTML = guessesLeft;
-	lettersGuessed = [];
-	document.getElementById('letters-guessed').innerHTML = lettersGuessed;
-}
-
+// Builds hangman word as user guesses letters.
 function buildWord(userInput) {
+
+	// Initialize placeholder array to underscore placeholder.
 	if (prevPlaceholderArray.length == 0) {
 		placeholderArray = createWordPlaceholder(word);
+
+	// Makes it possible to see letters and underscores.
 	} else {
 		placeholderArray = prevPlaceholderArray;
 	}
 
+	// Replace underscore with matching letter.
 	for (var i = 0; i < word.length; i++) {
 	  console.log('Word is ' + word);
 	  if (userInput == word[i]) {
@@ -102,43 +144,44 @@ function buildWord(userInput) {
 
 	prevPlaceholderArray = placeholderArray;
 
+	// Convert placeholder array to string for display in UI.
 	placeholder = placeholderArray.join(" ");
-	console.log("What's going on with placeholder? " + placeholder);
 	document.getElementById('word-placeholder').innerHTML = placeholder;
 
 	console.log("Placeholder Array length is " + placeholderArray.length);
 	console.log("Placeholder split is " + placeholder.split(","));
 	console.log("Word join is " + word.join(" "));
+	
+	// User wins when placeholder matches word.
 	if (placeholder.split(',') == word.join(" ")) {
 		console.log("Woot");
-		wins++
+		wins++;
 		document.getElementById('win-count').innerHTML = wins;
-		correctGuessCount = 0;
-		createWord(wordArray);
-		userInput = "";
-		prevPlaceholderArray = [];
 		restartGame();
 	}
-}
+};
 
-var correctGuessCount = 0;
-var guessesLeft = 13;
-//Function fires when letter selected.
-document.onkeyup = function(event) {
-	console.log('This is the key entered', event.key);
-	var keyPress;
+// Restart game, initializing several values.
+function restartGame(wordPlaceholder) {
+	
+	// Add new word.
+	createWord(wordArray);
 
-	if (typeof event != 'undefined') {
-		keyPress = event.keyCode;
-		userInput = String.fromCharCode(keyPress).toUpperCase();
-		console.log(userInput + " should match the key entered");
-		trackLetterGuesses(userInput);
-		buildWord(userInput);
-	}
-	else if (e) {
-		keyPress = e.which;
-	}
-	return false;
+	//Empty user input and placeholder values.
+    userInput = "";
+	prevPlaceholderArray = [];
+	placeholderArray = [];
+
+	// Reset remaining guesses.
+	guessesLeft = 13;
+
+	// Reset guess count.
+	correctGuessCount = 0;
+	document.getElementById('guess-count').innerHTML = guessesLeft;
+
+	// Reset list of letters guessed.
+	lettersGuessed = [];
+	document.getElementById('letters-guessed').innerHTML = lettersGuessed;
 };
 
 
